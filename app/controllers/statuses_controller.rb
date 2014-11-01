c90f02b69b0eed5254c279eedc7bd769c2bb841f
Kevin Chang <changkaimin.big@gmail.com>
2014-11-01 13:25:21 +0800

    First commit

---
+++ app/controllers/statuses_controller.rb
@@ -0,0 +1,24 @@
+class StatusesController < ApplicationController
+  def new
+    @status = Status.new
+  end
+
+  def create
+    @status = Status.new(status_params)
+    user = User.find(1)
+    @status.creator = user #TODO: update after authentication
+    
+    if @status.save
+      flash[:notice] = "Status created!"
+      redirect_to user_path(user.username)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def status_params
+    params.require(:status).permit(:body)
+  end
+end
